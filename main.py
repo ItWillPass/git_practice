c65221564f91eb4d9fcaa87c1fa9d7d3ae0e263b
Vlad Pavlov <burl5p9sdytf@mail.ru>
2022-11-30 20:05:43 +0300

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+value_1 = float(input("Enter first vlaue: "))
+value_2 = float(input("Enter second vlaue: "))
+operation = input('Enter operation: ')
+
+if operation == "/":
+    print(value_1 / value_2)
+elif operation == "-":
+    print(value_1 - value_2)
+elif operation == "*":
+    print(value_1 * value_2)
+elif operation == "+":
+    print(value_1 + value_2)
